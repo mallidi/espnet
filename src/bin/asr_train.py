76794d164337b9fad3ba975596f412f17269b542
Harish Mallidi <mallidih@amazon.com>
2018-08-02 21:32:06 -0400

    Added AttMultiHeadCnnAddv1       - first head is still a kernel of size=1       - second head has kernal size=3
    	modified:   src/bin/asr_train.py
    	modified:   src/nets/e2e_asr_attctc_th.py

--- src/bin/asr_train.py
+++ src/bin/asr_train.py
@@ -77,7 +77,8 @@ def main():
                         choices=['noatt', 'dot', 'add', 'location', 'coverage',
                                  'coverage_location', 'location2d', 'location_recurrent',
                                  'multi_head_dot', 'multi_head_add', 'multi_head_loc',
-                                 'multi_head_multi_res_loc', 'multi_head_cnn_add'],
+                                 'multi_head_multi_res_loc',
+                                 'multi_head_cnn_add', 'multi_head_cnn_add_v1'],
                         help='Type of attention architecture')
     parser.add_argument('--adim', default=320, type=int,
                         help='Number of attention transformation dimensions')
